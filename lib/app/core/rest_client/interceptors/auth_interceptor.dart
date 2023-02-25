5b72f6f738fdc290061cc9fa1a3ebc87543ebad6
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-24 22:25:50 -0300

    Criando Interceptor para requisições que precisem de autorização

---
+++ lib/app/core/rest_client/interceptors/auth_interceptor.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  Future<void> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final sp = await SharedPreferences.getInstance();
+    final accessToken = sp.getString('accessToken');
+
+    options.headers['Authorization'] = 'Bearer $accessToken';
+
+    handler.next(options);
+  }
+
+  @override
+  Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      //! Implementar erro de autorização
+    } else {
+      handler.next(err);
+    }
+  }
+}
