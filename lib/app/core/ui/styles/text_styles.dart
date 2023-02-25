b073237eb77ceeff33ac05f065368c5245a150f7
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-24 22:27:39 -0300

    Criando o tema do aplicativo

---
+++ lib/app/core/ui/styles/text_styles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static TextStyles? _instance;
+
+  TextStyles._();
+
+  static TextStyles get i {
+    _instance ??= TextStyles._();
+    return _instance!;
+  }
+
+  String get font => 'montserrat';
+
+  TextStyle get textLight => TextStyle(
+        fontWeight: FontWeight.w300,
+        fontFamily: font,
+      );
+
+  TextStyle get textRegular => TextStyle(
+        fontWeight: FontWeight.normal,
+        fontFamily: font,
+      );
+
+  TextStyle get textMedium => TextStyle(
+        fontWeight: FontWeight.w500,
+        fontFamily: font,
+      );
+
+  TextStyle get textSemiBold => TextStyle(
+        fontWeight: FontWeight.w600,
+        fontFamily: font,
+      );
+
+  TextStyle get textBold => TextStyle(
+        fontWeight: FontWeight.bold,
+        fontFamily: font,
+      );
+
+  TextStyle get textExtraBold => TextStyle(
+        fontWeight: FontWeight.w800,
+        fontFamily: font,
+      );
+
+  TextStyle get textButtonLabel => textBold.copyWith(fontSize: 14);
+
+  TextStyle get textTitle => textExtraBold.copyWith(fontSize: 28);
+}
+
+extension TextStyleExtension on BuildContext {
+  TextStyles get textStyle => TextStyles.i;
+}
