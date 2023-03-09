0d44d848e6697383c9d88802061d29b2b9bdc6fd
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-03-09 00:42:07 -0300

    Adicionando repositório de FriendsRepository e ChatRepository ao aplicationBinding

--- lib/app/core/provider/aplication_binding.dart
+++ lib/app/core/provider/aplication_binding.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:onlymessage/app/core/rest_client/custom_dio.dart';
 import 'package:onlymessage/app/repositories/auth/auth_repository.dart';
 import 'package:onlymessage/app/repositories/auth/auth_repository_impl.dart';
+import 'package:onlymessage/app/repositories/chat/chat_repository.dart';
+import 'package:onlymessage/app/repositories/chat/chat_repository_impl.dart';
+import 'package:onlymessage/app/repositories/friends/friends_repository.dart';
+import 'package:onlymessage/app/repositories/friends/friends_repository_impl.dart';
 import 'package:onlymessage/app/repositories/perfil_edit/perfil_edit_repository.dart';
 import 'package:onlymessage/app/repositories/perfil_edit/perfil_edit_repository_impl.dart';
 import 'package:onlymessage/app/repositories/user_to_add/user_to_add_repository.dart';
@@ -35,6 +39,16 @@ class AplicationBinding extends StatelessWidget {
         ),
         Provider<UserToAddRepository>(
           create: (context) => UserToAddRepositoryImpl(
+            dio: context.read(),
+          ),
+        ),
+        Provider<FriendsRepository>(
+          create: (context) => FriendsRepositoryImpl(
+            dio: context.read(),
+          ),
+        ),
+        Provider<ChatRepository>(
+          create: (context) => ChatRepositoryImpl(
             dio: context.read(),
           ),
         ),
