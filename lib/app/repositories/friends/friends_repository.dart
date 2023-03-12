fdc7e18da63ae945db38f1c200c4c9042baf5627
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-03-11 23:17:24 -0300

    Editando o repositório de friendsRepository

--- lib/app/repositories/friends/friends_repository.dart
+++ lib/app/repositories/friends/friends_repository.dart
@@ -1,5 +1,9 @@
 import 'package:onlymessage/app/models/friend.dart';
+import 'package:onlymessage/app/models/friend_request.dart';
 
 abstract class FriendsRepository {
   Future<List<Friend>> getFriends();
+  Future<List<FriendRequest>> getFriendsRequest();
+  Future<void> acceptFriendRequest(String id, bool hasAccept);
+  Future<void> removeFriend(String id);
 }
--- lib/app/repositories/friends/friends_repository_impl.dart
+++ lib/app/repositories/friends/friends_repository_impl.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dio/dio.dart';
 import 'package:onlymessage/app/core/exeptions/repository_exeption.dart';
 import 'package:onlymessage/app/core/rest_client/custom_dio.dart';
 import 'package:onlymessage/app/models/friend.dart';
+import 'package:onlymessage/app/models/friend_request.dart';
 
 import './friends_repository.dart';
 
@@ -21,10 +23,63 @@ class FriendsRepositoryImpl implements FriendsRepository {
 
       if (res.data != null) {
         list = data.map((e) => Friend.fromMap(e)).toList();
+
+        final hasLastMessage =
+            list.where((e) => e.lastMessage != null).toList();
+        final hasntLastMessage =
+            list.where((e) => e.lastMessage == null).toList();
+
+        if (hasLastMessage.length > 1) {
+          list.sort(
+            (x, y) => DateTime.parse(y.lastMessage?.sendeTime ?? '').compareTo(
+              DateTime.parse(x.lastMessage?.sendeTime ?? ''),
+            ),
+          );
+        }
+
+        list.clear();
+        list.addAll(hasLastMessage);
+        list.addAll(hasntLastMessage);
       }
+
       return list;
-    } on Exception catch (e) {
+    } on DioError catch (e) {
       throw RepositoryExeption(message: 'Erro ao buscar usuários.');
     }
+  }
+
+  @override
+  Future<List<FriendRequest>> getFriendsRequest() async {
+    try {
+      final res = await dio.auth().get('/friends/getfriendrequests');
+      List<dynamic> data = res.data;
+
+      List<FriendRequest> list = [];
+
+      if (res.data != null) {
+        list = data.map((e) => FriendRequest.fromMap(e)).toList();
+      }
+
+      return list;
+    } on DioError catch (e) {
+      throw RepositoryExeption(message: 'Erro ao buscar pedidos de amizade.');
+    }
+  }
+
+  @override
+  Future<void> acceptFriendRequest(String id, bool hasAccept) async {
+    try {
+      await dio.auth().post('/friends/friendship', data: {
+        'id': id,
+        'isAccepted': hasAccept,
+      });
+    } on DioError catch (e) {
+      throw RepositoryExeption(message: 'Erro ao buscar pedidos de amizade.');
+    }
+  }
+
+  @override
+  Future<void> removeFriend(String id) async {
+    await dio.auth().put('/friends/removefriendship/$id');
   }
 }
